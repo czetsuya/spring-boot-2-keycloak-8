dafbbd481e319b99a4c70562503f19bda3b879a2
Edward P. Legaspi <czetsuya@gmail.com>
2020-01-06 19:15:29 +0800

    doc: Update comment.

--- src/main/java/com/czetsuya/security/CustomKeycloakSpringBootConfigResolver.java
+++ src/main/java/com/czetsuya/security/CustomKeycloakSpringBootConfigResolver.java
@@ -1,20 +1,3 @@
-/**
- * An Open Source Inventory and Sales Management System
- * Copyright (C) 2019 Edward P. Legaspi (https://github.com/czetsuya)
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package com.czetsuya.security;
 
 import org.keycloak.adapters.KeycloakDeployment;
@@ -27,8 +10,8 @@ import org.springframework.context.annotation.Configuration;
 /**
  * @author Edward P. Legaspi | czetsuya@gmail.com
  * 
- * @since 0.0.2
- * @version 0.0.2
+ * @since 0.0.1
+ * @version 0.0.1
  */
 @Configuration
 public class CustomKeycloakSpringBootConfigResolver extends KeycloakSpringBootConfigResolver {
--- src/main/java/com/czetsuya/web/application/AccountController.java
+++ src/main/java/com/czetsuya/web/application/AccountController.java
@@ -1,20 +1,3 @@
-/**
- * An Open Source Inventory and Sales Management System
- * Copyright (C) 2019 Edward P. Legaspi (https://github.com/czetsuya)
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package com.czetsuya.web.application;
 
 import org.springframework.http.MediaType;
@@ -29,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
  * 
  * @author Edward P. Legaspi | czetsuya@gmail.com
  * 
- * @since
- * @version
+ * @since 0.0.1
+ * @version 0.0.1
  */
 @RestController
 @RequestMapping(path = "/iam/accounts", produces = MediaType.APPLICATION_JSON_VALUE)
